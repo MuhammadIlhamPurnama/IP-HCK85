d71a52d67440efb8c7da1cce6126115bad68a37f
Muhammad Ilham Purnama <ilhampurnama78@gmail.com>
2025-07-23 21:01:28 +0700

    chore: create middler owner only

---
+++ backend/middlewares/ownerOnly.js
@@ -0,0 +1,20 @@
+const { Thread } = require('../models')
+
+async function ownerOnly(req, res, next) {
+  try {
+    const { id } = req.params
+    const thread = await Thread.findByPk(id)
+    console.log(thread)
+    if (!thread) {
+      return res.status(404).json({ message: 'Thread not found' })
+    }
+    if (thread.UserId !== req.user.id) {
+      return res.status(403).json({ message: 'You are not the owner' })
+    }
+    next()
+  } catch (error) {
+    next(error)
+  }
+}
+
+module.exports = ownerOnly
